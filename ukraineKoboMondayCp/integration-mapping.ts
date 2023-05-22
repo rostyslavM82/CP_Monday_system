24b395134f047dc4480b097b431bd0805948e511
Ruslan <slovak82@gmail.com>
2023-05-22 12:40:17 +0300

    azure-CI-CD-test-2

--- ukraineKoboMondayCp/integration-mapping.ts
+++ ukraineKoboMondayCp/integration-mapping.ts
@@ -8,7 +8,7 @@ export const integrationMapping: IntegrationMappingItem[] = [
       },
       monday: {
         columnType: MondayColumnTypes.TEXT,
-        columnId: "text5-check-changes",
+        columnId: "text5-check-changes-test2",
       },
     }
 ]
