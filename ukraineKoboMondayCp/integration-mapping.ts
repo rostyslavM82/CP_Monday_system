400c77f095e80f71a091c7895fbbe531b85af0b4
Ruslan <slovak82@gmail.com>
2023-05-22 12:52:27 +0300

    integration-mapping-monday-test-1

--- ukraineKoboMondayCp/integration-mapping.ts
+++ ukraineKoboMondayCp/integration-mapping.ts
@@ -8,7 +8,7 @@ export const integrationMapping: IntegrationMappingItem[] = [
       },
       monday: {
         columnType: MondayColumnTypes.TEXT,
-        columnId: "text5-check-changes-test2",
+        columnId: "text3",
       },
     }
 ]
