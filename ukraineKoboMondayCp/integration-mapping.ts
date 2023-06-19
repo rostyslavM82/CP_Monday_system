d9d00cf94407b78df14a96564538f4d4ef856176
Ruslan <slovak82@gmail.com>
2023-06-19 14:56:03 +0300

    integration-maping-cp-finish

--- ukraineKoboMondayCp/integration-mapping.ts
+++ ukraineKoboMondayCp/integration-mapping.ts
@@ -21,6 +21,8 @@ export const integrationMapping: IntegrationMappingItem[] = [
     kobo: {
       questionType: KoboQuestionTypes.SELECT_ONE,
       questionId: "__005",
+      optionsListId: "ab1op10",
+      labelIndex: 1,
     },
     monday: {
       columnType: MondayColumnTypes.TEXT,
@@ -181,5 +183,339 @@ export const integrationMapping: IntegrationMappingItem[] = [
       { koboValue: "__1", mondayValue: "No" },
       { koboValue: CATCH_ALL_VALUE, mondayValue: "Unknown" },
     ],
+  },
+  //IDP status
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.SELECT_ONE,
+      questionId: "__015",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "status3",
+    },
+    optionMapping: [
+      { koboValue: "_", mondayValue: "Yes" },
+      { koboValue: "__1", mondayValue: "No" },
+      { koboValue: CATCH_ALL_VALUE, mondayValue: "Unknown" },
+    ],
+  },
+  //Сhild studies at
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.SELECT_ONE,
+      questionId: "_",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "status93",
+    },
+    optionMapping: [
+      { koboValue: "_", mondayValue: "School" },
+      { koboValue: "__1", mondayValue: "Kindergarten" },
+      { koboValue: "__2", mondayValue: "Other" },
+      { koboValue: CATCH_ALL_VALUE, mondayValue: "Unknown" },
+    ],
+  },
+  //Grade the child study in
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__016",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text_113",
+    },
+  },
+  //Name of the educational institution
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__017",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text_181",
+    },
+  },
+  //Kindergarten group the child study in
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__018",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text_12",
+    },
+  },
+  //Name of the educational institution
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__019",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text_121",
+    },
+  },
+  //Other place name
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__020",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text_13",
+    },
+  },
+  //Name and Surname of parents or trustees
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__021",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text33",
+    },
+  },
+  //Contact phone number of parents/guardians
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "_380",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text13",
+    },
+  },
+  //Name of the siblings who attend the Community Center "Child, Youth and Family Friendly Space"
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__022",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text7",
+    },
+  },
+  //Name and surname in case of trust person in emergency if parents/guardians are not available
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__023",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text_15",
+    },
+  },
+  //Contact phone number if trust person in case of emergency if parents/guardians are not available
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.INTEGER,
+      questionId: "_380_001",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text79",
+    },
+  },
+  //Does the child have a disability?
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.SELECT_ONE,
+      questionId: "__024",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "status9",
+    },
+    optionMapping: [
+      { koboValue: "_", mondayValue: "No" },
+      { koboValue: "__1", mondayValue: "Yes" },
+      { koboValue: CATCH_ALL_VALUE, mondayValue: "Unknown" },
+    ],
+  },
+  //Does the child have any special needs/medical conditions that need to be known to the specialists during the activities?
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.SELECT_ONE,
+      questionId: "__024",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "status8",
+    },
+    optionMapping: [
+      { koboValue: "_", mondayValue: "No" },
+      { koboValue: "__1", mondayValue: "Yes" },
+      { koboValue: CATCH_ALL_VALUE, mondayValue: "Unknown" },
+    ],
+  },
+  //Indicate any special needs/medical conditions that the specialists need to know during the event
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__001",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text_184",
+    },
+  },
+  //Special needs/medical conditions arise before or after the outbreak of hostilities?
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.SELECT_ONE,
+      questionId: "__025",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "status0",
+    },
+    optionMapping: [
+      { koboValue: "_", mondayValue: "Before" },
+      { koboValue: "__1", mondayValue: "After" },
+      { koboValue: CATCH_ALL_VALUE, mondayValue: "Unknown" },
+    ],
+  },
+  //Programs in which the child will participate, by choosing a program you give the consent of the parents/guardians to the child's participation in the programs
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.SELECT_MULTIPLE,
+      questionId: "__003",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "dropdown",
+    },
+  },
+  //The event has been initiated:
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.SELECT_ONE,
+      questionId: "__004",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "status2",
+    },
+    optionMapping: [
+      { koboValue: "sci", mondayValue: "SCI" },
+      { koboValue: "__", mondayValue: "Partner" },
+      { koboValue: CATCH_ALL_VALUE, mondayValue: "Unknown" },
+    ],
+  },
+  //Partner's name
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__026",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "text95",
+    },
+  },
+  //Activity's name
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__027",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text6",
+    },
+  },
+  //Name of the SCI specialist
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__030",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text5",
+    },
+  },
+  //Partner/SCI
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.SELECT_ONE,
+      questionId: "_SCI",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "color",
+    },
+    optionMapping: [
+      { koboValue: "__", mondayValue: "Partner" },
+      { koboValue: "sci", mondayValue: "SCI" },
+      { koboValue: CATCH_ALL_VALUE, mondayValue: "Unknown" },
+    ],
+  },
+  //Partner Name
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.SELECT_ONE,
+      questionId: "_SCI",
+    },
+    monday: {
+      columnType: MondayColumnTypes.STATUS,
+      columnId: "color",
+    },
+    optionMapping: [
+      {
+        koboValue: "mariupol_city_center_of_support_and_deve",
+        mondayValue: "Mariupol City Center of Support and Development of SMEs",
+      },
+      { koboValue: "dropwd", mondayValue: "DROPWD" },
+      { koboValue: "civil_initiatives", mondayValue: "Civil Initiatives" },
+      { koboValue: "cf__slavic_heart", mondayValue: "CF Slavic Heart" },
+      {
+        koboValue: "league_of_modern_women",
+        mondayValue: "League of modern women",
+      },
+      { koboValue: "bilozerka", mondayValue: "Bilozerka" },
+      { koboValue: "poruch", mondayValue: "Poruch" },
+      {
+        koboValue: "pectoral__zelo_art_centre",
+        mondayValue: "Pectoral- Zelo Art Centre",
+      },
+      { koboValue: "red_cross", mondayValue: "Red Cross" },
+      { koboValue: "cf_posmishka_ua", mondayValue: "CF Posmishka UA" },
+      { koboValue: "_", mondayValue: "Other" },
+      { koboValue: CATCH_ALL_VALUE, mondayValue: "Unknown" },
+    ],
+  },
+  //Other partner organization's name
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__002",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text9",
+    },
+  },
+  //Position of the representative
+  {
+    kobo: {
+      questionType: KoboQuestionTypes.TEXT,
+      questionId: "__031",
+    },
+    monday: {
+      columnType: MondayColumnTypes.TEXT,
+      columnId: "text06",
+    },
   },
 ];
